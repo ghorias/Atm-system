ad6c142011eb1dc2778a6dd5b27da2f4e0e99e1e
Kristian <kgharios@kth.se>
2026-04-11 23:16:32 +0200

    Jag har gjort själva objektet Account.java där konton skapas med hashat lösenord, ID, kontonummer, saldo. Konstruktor och tom konstruktor finns också. Den är skapad för att kunna prata med en databas, men kan också hardkoda data.

--- backend/src/main/java/com/example/demo/model/Account.java
+++ backend/src/main/java/com/example/demo/model/Account.java
@@ -1,11 +1,14 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
 
+
+//Denna klass är själva objektet.
 @Entity
 public class Account {
     //fält
@@ -13,11 +16,14 @@ public class Account {
     //sen en PIN-kod/hash
     @GeneratedValue
     @Id
-    private long ID;
+    private long ID;//teknisk databasidentitet för id.
 
-    private String kontonummer;
+    @Column(unique = true) // så varje konto har ett unikt kontonummer.
+    private String kontonummer;//affärsidentitet är kontonummer
 
     private BigDecimal saldo;
+
+    private String lösenordHash;
 
     //Annoteringar, för databas bör vi spara data i med denna klass
     //med @Entity kolla up det och @Id vad de gör. De ska sitta ovanför klassnamnet resp ID-variabler
@@ -32,14 +38,30 @@ public class Account {
         return kontonummer;
     }
 
-    public BigDecimal getSaldo() {
+    private BigDecimal getSaldo() {
         return saldo;
     }
 
-    //setters
-
-
-    public void setSaldo(BigDecimal saldo) {
-        this.saldo = saldo;
+    public String getLösenordHash() {
+        return lösenordHash;
     }
+
+    //setters
+    public void setLösenordHash(String lösenord) {
+        this.lösenordHash = lösenord;
+    }
+
+    //KONSTRUKTOR, med denna kan vi göra ett nytt account smidigt.
+    // Ex: Account Isak = new Account("12345678", new BigDecimal("1000.00"), "lösenord123");
+    public Account(String kontonummer, BigDecimal saldo, String lösenord) {
+        this.kontonummer = kontonummer;
+        this.saldo = saldo;
+        this.lösenordHash = lösenord;
+    }
+
+    public Account(){
+
+    }
+
+
 }
