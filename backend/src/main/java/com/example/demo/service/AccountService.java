e65dfcc1397b702b767461d411a2a1c5b588cc92
Kristian <kgharios@kth.se>
2026-04-27 16:46:11 +0200

    Made the business logic, withdraw, deposit and showbalance.

--- backend/src/main/java/com/example/demo/service/AccountService.java
+++ backend/src/main/java/com/example/demo/service/AccountService.java
@@ -1,18 +1,57 @@
 package com.example.demo.service;
-
 import java.math.BigDecimal;
 
+import com.example.demo.model.Account;
+import com.example.demo.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
 
+    private final AccountRepository accountRepository;
 
-    public void deposit(){
-
-    }
-    public void withrdraw(){
-
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
     }
 
-    public void showBalance(){
+    //variable name could change "findByAccountNumber"
+    public BigDecimal deposit(String accountNumber, BigDecimal amount){
+        //This gives us acces to the information of the account, stored in variable "account"
+        Account account = accountRepository.findByAccountNumber(accountNumber);
+
+        //Here is where the calculation is happening, getting the balance from set account doing the transaction and adding
+        //the deposit amount to the account balance. Everything is stored in the newBalance Variable
+        BigDecimal newBalance = account.getBalance().add(amount);
+
+        account.setBalance(newBalance);
+
+        //Here we save the new balance.
+        accountRepository.update(account);
+
+       return newBalance;
+    }
+
+
+    public BigDecimal withdraw(String accountNumber, BigDecimal amount){
+        Account account = accountRepository.findByAccountNumber(accountNumber);
+        BigDecimal newBalance;
+        BigDecimal balance = accountRepository.findByAccountNumber(accountNumber).getBalance();
+
+
+        if(amount.compareTo(balance) > 0){
+            throw new IllegalArgumentException("Insufficient balance");
+        }
+        newBalance = balance.subtract(amount);
+
+        accountRepository.update(account);
+
+        return newBalance;
+
+    }
+
+    public BigDecimal showBalance(String Account){
+        return accountRepository.findByAccountNumber(accountNumber).getBalance();
+
 
     }
 
